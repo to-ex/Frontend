804eb72856f49e755943b4a91c4f29cbce25ab78
kkimhyorr <hyorie03@naver.com>
2024-07-03 00:19:27 +0900

    Feat: Mywrotepage 구현

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ScrapPage from "./pages/ScrapPage";
 import { ThemeProvider } from "styled-components";
 import { Theme } from "./styles/Theme";
+import MyWrotePage from "./pages/MyWrotePage";
 
 function App() {
   return (
     <ThemeProvider theme={Theme}>
       <Router>
         <Routes>
-          <Route path="/" element={<ScrapPage />} />
+          <Route path="/scrap" element={<ScrapPage />} />
+          <Route path="/mywrote" element={<MyWrotePage />} />
         </Routes>
       </Router>
     </ThemeProvider>
