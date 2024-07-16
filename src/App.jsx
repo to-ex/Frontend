871a352e4467739128beb1a1429900233a36b8dc
kkimhyorr <hyorie03@naver.com>
2024-07-16 20:19:41 +0900

    Chore: moment 설치

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import React from "react";
 import "./App.css";
 import { ThemeProvider } from "styled-components";
 import { Theme } from "./styles/Theme";
+import CustomCalendar from "./components/CustomCalendar";
 
 function App() {
-  return <ThemeProvider theme={Theme}></ThemeProvider>;
+  return (
+    <ThemeProvider theme={Theme}>
+      <CustomCalendar />
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/components/CustomCalendar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Calendar from "react-calendar";
+
+const CustomCalendar = () => {
+  return (
+    <>
+      <Calendar />
+    </>
+  );
+};
+
+export default CustomCalendar;
