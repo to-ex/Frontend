bf9cc9ba418cb902f3d6a8810464109f3a7eceb6
heeyyeon <wjdgmldus28@gachon.ac.kr>
2024-07-21 13:00:42 +0900

    Feat: 라우터 연결

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <ThemeProvider theme={Theme}>
       <Router>
       <Routes>
-        <Route path="/LanguagePage" element={<LanguagePage />} />
+        <Route path="/Language" element={<LanguagePage />} />
       </Routes>
     </Router>
     </ThemeProvider>
